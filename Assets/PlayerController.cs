1b770a0e513272f5f13a275fc49658b843b2748f
Javier Ortega <fjortegan@gmail.com>
2024-12-04 09:58:37 +0100

    cinemachine camera

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,16 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 2.0f;
     [SerializeField] private float jumpForce = 5.0f;
+    [SerializeField] private Transform rayCastPosition;
 
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D rigidBody2D;
+    private Animator animator;
     private bool onFloor = false;
+    private bool onAir = false;
 
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         rigidBody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,18 +28,52 @@ public class PlayerController : MonoBehaviour
         float inputAxis = Input.GetAxis("Horizontal");
         transform.position += new Vector3(inputAxis * Time.deltaTime * speed,
                                           0, 0);
-        spriteRenderer.flipX = inputAxis == .0f? spriteRenderer.flipX : inputAxis > 0;
 
-        var rayCast = Physics2D.Raycast(transform.position - (transform.up * 1.1f),
-                                        -transform.up , .1f);
-        if(rayCast.collider != null) onFloor = true;
-            else onFloor = false;
+        spriteRenderer.flipX = inputAxis == .0f? Standing() : Walking(inputAxis);
+
+        FloorDetection();
 
         if (onFloor && Input.GetButtonDown("Jump"))
         {
             rigidBody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             onFloor = false;
+            animator.SetBool("isJumping", true);
+            onAir = true;
+            StartCoroutine(InitJump());
         }
+    }
+
+    private IEnumerator InitJump()
+    {
+        yield return new WaitForSeconds(0.1f);
+        onAir = false;
+    }
+
+    private bool Walking(float inputAxis)
+    {
+        animator.SetBool("isWalking", true);
+        return inputAxis > 0;
+    }
+
+    private bool Standing()
+    {
+        animator.SetBool("isWalking", false);
+        return spriteRenderer.flipX;
+    }
+
+    private void FloorDetection()
+    {
+        var rayCast = Physics2D.Raycast(rayCastPosition.position, -transform.up, .1f);
+
+        if (rayCast.collider != null)
+        {
+            if (!onAir)
+            {
+                onFloor = true;
+                animator.SetBool("isJumping", false);
+            }
+        }
+        else onFloor = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
